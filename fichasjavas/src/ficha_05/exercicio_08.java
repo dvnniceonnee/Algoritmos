3285f58beb5e851202101e07782c700a4608b130
dvnniceonnee <160496839+dvnniceonnee@users.noreply.github.com>
2024-03-08 14:02:18 +0000

    alterações

--- fichasjavas/src/ficha_05/exercicio_08.java
+++ fichasjavas/src/ficha_05/exercicio_08.java
@@ -5,25 +5,21 @@ import java.util.Scanner;
 public class exercicio_08 {
     public static void main(String[] args) {
         // Importar o Scanner
-        Scanner  input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         // Declarar variaveis
         int[][] matrizNumeros = new int[3][3];
 
         // Ler as variaveis
-        for (int i = 0; i < matrizNumeros.length ;i++ )
-        {
-            for(int e = 0 ; e < matrizNumeros[0].length ; e++)
-            {
+        for (int i = 0; i < matrizNumeros.length; i++) {
+            for (int e = 0; e < matrizNumeros[0].length; e++) {
                 System.out.print("Introduza um numero inteiro (0-9) : ");
                 matrizNumeros[i][e] = input.nextInt();
             }
         }
 
-        for (int i = 0; i < matrizNumeros.length ;i++ )
-        {
-            for(int e = 0 ; e < matrizNumeros[0].length ; e++)
-            {
+        for (int i = 0; i < matrizNumeros.length; i++) {
+            for (int e = 0; e < matrizNumeros[0].length; e++) {
                 System.out.print(matrizNumeros[i][e] + " ");
             }
             System.out.println(" ");
