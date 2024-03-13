0ee02122ec1ea1497c13b696e77ae92f70d9c24f
dvnniceonnee <160496839+dvnniceonnee@users.noreply.github.com>
2024-03-13 16:57:26 +0000

    aula algo 13-03

--- fichasjavas/src/ficha_07/teste.java
+++ fichasjavas/src/ficha_07/teste.java
@@ -3,12 +3,16 @@ package ficha_07;
 import com.sun.security.jgss.GSSUtil;
 
 import java.sql.SQLOutput;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class teste {
     public static void main(String[] args) {
         int[] numeros = {5, 1, 2, 3, 4, 5, 17, 19};
-        int[] divisores = {6, 0, 6 , 0, 9, 10};
+        int[] divisores = {6, 0, 6, 0, 9, 10};
 
+        Scanner input = new Scanner(System.in);
+/*
         for (int i = 0; i < numeros.length; i++) {
             try {
                 System.out.println("Divisão " + i);
@@ -28,16 +32,17 @@ public class teste {
             System.out.println("Divisao :" + numeros[i] / divisores[i]);
             System.out.println("Elemento nao encontado!");
         }
+        */
 
     }
-
 }
 
-class MinhaExcepcao extends Exception{
-    MinhaExcepcao(String letra){
-        try{
-            String novaString = letra;
+class MinhaExcepcao extends Exception {
+    MinhaExcepcao(int num) {
+        try {
+            int numero = num;
+        } catch (InputMismatchException ex1) {
+            System.out.println("Numero invalido!");
         }
-        catch (Str)
     }
 }
