7051fbe314740e6d5245f4f80c00566a15816e13
dvnniceonnee <160496839+dvnniceonnee@users.noreply.github.com>
2024-03-22 23:37:41 +0000

    trabalho

--- fichasjavas/src/trabalhoAvaliacao/bibliotecaFunc.java
+++ fichasjavas/src/trabalhoAvaliacao/bibliotecaFunc.java
@@ -21,7 +21,7 @@ public class bibliotecaFunc {
         String[][] matriz = new String[0][0];
         try {
             int[] numberLinCol = numeroLinhasEcolunas(ficheiro, charDelimitador);
-            matriz = turnFileIntomatriz(ficheiro, numberLinCol[0], numberLinCol[1], cabecalhoRetirar, charDelimitador);
+            matriz = turnFileIntoMatriz(ficheiro, numberLinCol[0], numberLinCol[1], cabecalhoRetirar, charDelimitador);
         } catch (FileNotFoundException ex1) {
             System.out.println("Ficheiro " + ficheiro.getName() + " nao foi lido!");
         }
@@ -69,7 +69,7 @@ public class bibliotecaFunc {
      * @param cabecalho booleanda para perguntar se quer retirar o cabecalho (true = retira , false = nao retira)
      * @return uma matriz com a informação toda do ficheiro
      */
-    public static String[][] turnFileIntomatriz(File ficheiro, int linhas, int colunas, boolean cabecalho, String delimitador) {
+    public static String[][] turnFileIntoMatriz(File ficheiro, int linhas, int colunas, boolean cabecalho, String delimitador) {
         String[][] matrizFile = new String[0][0];
         try {
             Scanner scanFile = new Scanner(ficheiro);
@@ -103,7 +103,7 @@ public class bibliotecaFunc {
     public static boolean searchIfExistsOnMatriz(String[][] matriz, String dataInfo, int colunaDadoPesquisar) {
         boolean clienteExiste = false;
         for (int i = 0; i < matriz.length; i++) {
-            if (matriz[i][colunaDadoPesquisar].equals(dataInfo)) { // caso o dado na coluna "i" seja igual ao dado esse mesmo dado existe
+            if (matriz[i][colunaDadoPesquisar].equalsIgnoreCase(dataInfo)) { // caso o dado na coluna "i" seja igual ao dado esse mesmo dado existe
                 clienteExiste = true;
                 i = matriz.length; // termina o loop porque ja verificamos que existe
             }
@@ -128,6 +128,133 @@ public class bibliotecaFunc {
             }
         }
         return matriz[linhaInfo];
+    }
+
+    /**
+     * Função para pesquisar uma matriz por um dado em cada linha e retorna uma matriz só com as linhas em que tenham esse dado
+     *
+     * @param matriz          matriz que vamos pesquisar o dado
+     * @param searchData      dado que vamos usar para pesquisar
+     * @param colDataToSearch coluna da matriz onde estarà esse dado
+     * @return matriz filtrada com a pesquisa de dados
+     */
+    public static String[][] searchForDataMatriz(String[][] matriz, String searchData, int colDataToSearch) {
+        int linhaInfo = 0;
+        String[][] lineReturn = new String[0][0];  // declaração da matriz que vai ser declarada
+        for (int i = 0; i < matriz.length; i++) {
+            if (matriz[i][colDataToSearch].equalsIgnoreCase(searchData)) {                         // caso o dado que estamos á procura esteja na linha da matriz durante a iteração
+                String[][] temp = new String[lineReturn.length + 1][matriz[0].length];   // matriz temporaria com o acrescimo de uma linha
+                if (lineReturn.length == 0) {                                            // caso a matriz que vai ser retornada ainda nao tiver linhas só adicionamos a linha
+                    for (int k = 0; k < temp[0].length; k++) {
+                        temp[0][k] = matriz[i][k];
+                    }
+                } else {
+                    for (int j = 0; j < lineReturn.length; j++) {                         // matriz já contem mais do que uma linha portanto vamos iterar a matriz e adicionar tudo o que ela tem menos a linha nova
+                        for (int k = 0; k < lineReturn[0].length; k++) {
+                            temp[j][k] = lineReturn[j][k];
+                        }
+                    }
+                    for (int k = 0; k < lineReturn[0].length; k++) {
+                        temp[lineReturn.length][k] = matriz[i][k];                        // adiciono a linha nova em ultimo lugar
+                    }
+                }
+                lineReturn = temp;
+            }
+        }
+        return lineReturn;
+    }
+
+    /**
+     * Função que retorna uma matriz com a linha de dados adicionada
+     *
+     * @param arrayToAdd  dados a adicionar á matriz em forma de array (numero de colunas tem de ser o mesmo do que o numero de colunas da matriz)
+     * @param originalMatriz matriz onde vao ser adicionados os dados (matriz pode conter 0 linhas)
+     * @return uma matriz com os dados novos inseridos
+     */
+    public static String[][] addLineToMatriz(String[] arrayToAdd, String[][] originalMatriz) {
+        String[][] temp = new String[originalMatriz.length + 1][arrayToAdd.length];
+        if (originalMatriz.length > 0) {
+            for (int i = 0; i < originalMatriz.length; i++) {
+                for (int k = 0; k < originalMatriz[0].length; k++) {
+                    temp[i][k] = originalMatriz[i][k];
+                }
+            }
+        }
+        for (int i = 0; i < arrayToAdd.length; i++) {
+            temp[originalMatriz.length][i] = arrayToAdd[i];
+        }
+        return temp;
+    }
+
+    /**
+     * Função para adicionar um elemento novo a uma array
+     * @param originalArray array onde vai ser adicionado um novo elemento
+     * @param elementToAdd String com o elemento a ser adicionado
+     * @return array com mais uma linha em que vai conter o novo elemento
+     */
+    public static String[] addElementToArray(String[] originalArray, String elementToAdd) {
+        String[] temp = new String[originalArray.length + 1];
+        if (originalArray.length == 0 ){
+            temp[0] = elementToAdd;
+        }
+        else {
+            for (int i = 0; i < originalArray.length; i++){
+                temp[i] = originalArray[i];
+            }
+            temp[originalArray.length] = elementToAdd;
+        }
+        return temp;
+    }
+
+    /**
+     * Função que permite remover uma linha de uma matriz (<u>necessita de verificar antes se a linha existe - <strong color="blue">searchIfExistsOnMatriz</strong></u>)
+     *
+     * @param matriz     de dados de onde vai ser removida a linha
+     * @param dado       informação que vai estar na linha que vai ser removida
+     * @param colunaDado coluna onde vai estar o dado que foi dado por parametro
+     * @return uma matriz nova com a linha apagada
+     */
+    public static String[][] deleteLine(String[][] matriz, String dado, int colunaDado) {
+        String[][] temp = new String[matriz.length - 1][matriz[0].length];
+        int countingLines = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            if (matriz[i][colunaDado].equals(dado)) {    // caso encontre a linha onde está o dado da linha que é para remover nao a adiciona à nova matriz
+                countingLines++;
+            } else {
+                for (int k = 0; k < matriz[0].length; k++) {
+                    temp[i - countingLines][k] = matriz[i][k];
+                }
+            }
+        }
+        return temp;
+    }
+
+    /**
+     * Função que retira os dados de uma coluna e os representa num array sem repetições
+     *
+     * @param matriz - recebe uma matriz preenchida
+     * @param num    - recebe o numero da coluna que queremos retirar os dados
+     * @return um array com os dados sem repetição dos mesmos
+     */
+    public static String[] columnDataWithoutRep(String[][] matriz, int num) {
+        String[] listaGen = new String[0];
+        for (int i = 0; i < matriz.length; i++) {
+            if (i == 0) {  // o primeiro elemento que encontrar entra na array porque ainda nao há repetições
+                listaGen = addElementToArray(listaGen, matriz[i][num]);
+            } else {
+                boolean entra = true;
+                for (int k = 0; k < listaGen.length; k++) { // como existe mais do que 1 elemento no array neste ponto ele verifica se o elemento da coluna que estamos a pesquisar já existe
+                    if (listaGen[k].equals(matriz[i][num])) {
+                        entra = false;  // se já existir na array o booleano fica true
+                        k = listaGen.length;
+                    }
+                }
+                if (entra) {
+                    listaGen = addElementToArray(listaGen, matriz[i][num]);
+                }
+            }
+        }
+        return listaGen;
     }
 
 }
--- fichasjavas/src/trabalhoAvaliacao/main.java
+++ fichasjavas/src/trabalhoAvaliacao/main.java
@@ -37,6 +37,7 @@ public class main {
 
     /**
      * Método que verifica se todos os ficheiros iniciais necessários estão válidos
+     *
      * @throws FileNotFoundException
      */
     public static void checkAllFiles() throws FileNotFoundException {
@@ -45,21 +46,26 @@ public class main {
             fileScanner.close();                                                    // fecha o scanner para o próximo ficheiro
         }
     }
-    public static boolean loginAdminValid(Scanner input){
+
+    public static boolean loginAdminValid(Scanner input) {
         System.out.print("Introduza o username : ");
         String username = input.next() + input.nextLine();
         System.out.print("Introduza a password : ");
         String password = input.nextLine();
-        if (searchIfExistsOnMatriz(matrizLogins, username, 0)){
+        if (searchIfExistsOnMatriz(matrizLogins, username, 0)) {
             String passwordUser = searchForDataArray(matrizLogins, username, 0)[1];
-            if (passwordUser.equals(password)){
+            if (passwordUser.equals(password)) {
                 return true;
             }
         }
         return false;
     }
 
-    public static String opcoesMenuAdmin(){
+    // ------------------- Secção Administradores -------------------------
+    // ------------------- Secção Administradores -------------------------
+    // ------------------- Secção Administradores -------------------------
+    // ------------------- Secção Administradores -------------------------
+    public static String opcoesMenuAdmin() {
         String menu = "\n1. Consulta de Ficheiros" +
                 "\n2. Total de vendas" +
                 "\n3. Total de Lucro" +
@@ -76,16 +82,16 @@ public class main {
 
     public static void menuConsultaFicheiros(Scanner input) throws FileNotFoundException {
         int opcao = 0;
-        do{
+        do {
             System.out.println("1. Imprimir ficheiro das vendas\n2. Imprimir ficheiro dos clientes\n3. Imprimir ficheiro das categorias\n4. Sair");
-            try{
+            try {
                 opcao = input.nextInt();
-            }catch (InputMismatchException ex1){
+            } catch (InputMismatchException ex1) {
                 opcao = 0;
                 input.next();
             }
-            switch (opcao){
-                case 1 :
+            switch (opcao) {
+                case 1:
                     printFile(filesPaths()[2]);
                     break;
                 case 2:
@@ -97,70 +103,181 @@ public class main {
                 default:
                     System.out.println("Opção inválida!");
             }
-        }while (opcao != 4);
+        } while (opcao != 4);
     }
 
-    public static double profitVenda(String idVenda){
-        String[] infoVenda = searchForDataArray(matrizVendas,idVenda, 0 );
-        double percentagemLucro = Double.parseDouble(searchForDataArray(matrizCategorias, infoVenda[3], 0)[1]);
+    /**
+     * Função que retorna o valor total faturado da venda e o lucro da mesma
+     *
+     * @param idVenda o numero da venda para calcular o lucro
+     * @return array com os valores <br>
+     * [0] = valor faturado total faturado na venda <br>
+     * [1] = valor do lucro sobre a venda
+     */
+    public static double[] statisticsVenda(String idVenda) {
+        String[] infoVenda = searchForDataArray(matrizVendas, idVenda, 0);
+        double percentagemLucro = Double.parseDouble(searchForDataArray(matrizCategorias, infoVenda[3], 0)[1]);  // procura a percentagem de margem da categoria da venda
         double valorVenda = Double.parseDouble(infoVenda[5]);
-        return  valorVenda * (percentagemLucro/100);
+        double lucro = valorVenda * (percentagemLucro / 100);
+        double[] info = {valorVenda, lucro};
+        return info;
     }
+
+    /**
+     * Metodo que retorna as estatisticas das vendas
+     *
+     * @return array de doubles
+     * <br> [0] = valor total faturado
+     * <br> [1] = valor total de lucro
+     * <br> [2] = Nome do jogo mais caro
+     * <br> [3] = lucro total da melhor categoria
+     * <br> [4] = nome da categoria mais lucrativa
+     */
+    public static String[] statisticsAllVendas() {
+        double valorfaturado = 0;
+        double lucroTotal = 0;
+        double jogoMaisCaroValor = 0;
+        String jogoMaisCaro = "";
+        double lucroTotalCategoria = 0;
+        String melhorCategoria = "";
+        boolean lucroVendas = false;
+        for (int i = 0; i < matrizCategorias.length; i++) {
+            double categoriaLucro = 0;
+            for (int k = 0; k < matrizVendas.length; k++) {
+                if(!lucroVendas){                   // parte na iteração só necessita de correr uma primeira vez para calcular o lucro total e o total de faturamento nas vendas da loja
+                    valorfaturado += Double.parseDouble(matrizVendas[k][5]);    // valor da venda na linha "k" que vai adicionar ao valor total faturado da loja
+                    lucroTotal += statisticsVenda(matrizVendas[k][0])[1];       // lucro da venda na linha "k" que vai adicionar ao lucro total faturado da loja
+                    if (valorfaturado > jogoMaisCaroValor) {                    // condicional para verificar qual é o jogo mais caro
+                        jogoMaisCaroValor = valorfaturado;
+                        jogoMaisCaro = matrizVendas[k][4];
+                    }
+                }
+                    // usamos este iterador para verificar qual a categoria que deu mais lucro
+                if (matrizVendas[k][3].equals(matrizCategorias[i][0])) {            // caso a categoria na linha "i" seja igual à da venda adiciona ao lucro desta categoria
+                    categoriaLucro += statisticsVenda(matrizVendas[k][0])[1];
+                }
+                if (categoriaLucro > lucroTotalCategoria) {
+                    lucroTotalCategoria = categoriaLucro;
+                    melhorCategoria = matrizCategorias[i][0];
+                }
+            }
+            lucroVendas = true;
+        }
+        String[] statistics = {String.valueOf(valorfaturado), String.valueOf(lucroTotal), jogoMaisCaro, String.valueOf(lucroTotalCategoria), melhorCategoria};
+        return statistics;
+    }
+
+    /**
+     * Função que retorna uma lista dos id's dos clientes por ordem descrescente em termos de gastos na loja assim como o gasto de cada um
+     *
+     * @return matriz por ordem decrescente (coluna [0] = id do cliente <br> coluna [1] = valor gasto pelo id cliente dessa linha) <br>
+     * 1º Linha da matriz contém o cliente que mais gastou e assim sucessivamente
+     */
+    public static String[][] listaClientesOrdemGasto() {
+        String[][] clientesPorOrdemGasto = new String[0][];
+        String[][] temp = matrizClientes;
+        for (int i = 0; i < matrizClientes.length; i++) {  // basicamente vamos iterar todos os clientes porque queremos uma lista por ordem decrescente dos gastos de todos os clientes
+            double valorTotalGasto = 0;  //
+            String idClienteMaisGastador = "";
+            for (int k = 0; k < temp.length; k++) {
+                String[][] matrizVendasDoCliente = searchForDataMatriz(matrizVendas, temp[k][0], 1);
+                double valorGastoCliente = 0;
+                for (int e = 0; e < matrizVendasDoCliente.length; e++) {
+                    valorGastoCliente += statisticsVenda(matrizVendasDoCliente[e][0])[0];
+                }
+                if (valorGastoCliente > valorTotalGasto) {
+                    valorTotalGasto = valorGastoCliente;
+                    idClienteMaisGastador = temp[k][0];
+                }
+            }
+            String[] clienteGastos = {idClienteMaisGastador, String.valueOf(valorTotalGasto)};
+            clientesPorOrdemGasto = addLineToMatriz(clienteGastos, clientesPorOrdemGasto);
+            temp = deleteLine(temp, idClienteMaisGastador, 0);
+        }
+        return clientesPorOrdemGasto;
+    }
+
     public static void menuAdmin(Scanner input) throws FileNotFoundException {
         int opcaoAdmin = -1;
-        do{
+        do {
             System.out.println(opcoesMenuAdmin());
             System.out.print("Introduza o que deseja efetuar : ");
-            try{
+            try {
                 opcaoAdmin = input.nextInt();
-            }catch (InputMismatchException ex1){
+            } catch (InputMismatchException ex1) {
                 opcaoAdmin = -1;
             }
-            switch (opcaoAdmin){
+            switch (opcaoAdmin) {
                 case 0:
                     break;
-                case 1 :
+                case 1:
                     menuConsultaFicheiros(input);
                     break;
                 case 2:
-                    double valorTotal = 0;
-                    for (int i = 0; i < matrizVendas.length; i ++){
-                        valorTotal += Double.parseDouble(matrizVendas[i][5]);
-                    }
                     System.out.println("\n Foram feitas " + matrizVendas.length + " vendas!");
-                    System.out.println(" Com um valor total de : " + valorTotal + "\n");
+                    System.out.println(" Com um valor total de : " + statisticsAllVendas()[0] + "\n");
                     break;
                 case 3:
-                    double lucroTotal = 0;
-                    for (int i = 0; i < matrizVendas.length; i++){
-                        lucroTotal += profitVenda(matrizVendas[i][0]);
-                    }
-                    System.out.println("\n Lucro total : " + lucroTotal);
+                    System.out.println("\n Lucro total das vendas: " + statisticsAllVendas()[1]);
                     break;
                 case 4:
                     int idUtilizador = 0;
-                    do{
+                    do {
                         System.out.print("Introduza o id de cliente : ");
-                        try{
+                        try {
                             idUtilizador = input.nextInt();
-                            if(!searchIfExistsOnMatriz(matrizClientes, String.valueOf(idUtilizador), 0)){
-                                System.out.println("Cliente inexistente!");
+                            if (!searchIfExistsOnMatriz(matrizClientes, String.valueOf(idUtilizador), 0)) {
+                                System.out.println(espacos + "!!!!  Cliente inexistente  !!!!");
                                 idUtilizador = 0;
                             }
-                        }catch (InputMismatchException ex1){
-                            System.out.println("Id Introduzido invalido!");
+                        } catch (InputMismatchException ex1) {
+                            input.next();
+                            System.out.println(espacos + "!!!! ID Introduzido inválido !!!!");
                         }
-                    }while (idUtilizador == 0);
+                    } while (idUtilizador == 0);
                     String[] infoClient = searchForDataArray(matrizClientes, String.valueOf(idUtilizador), 0);
-                    System.out.println("\n ID : " + infoClient[0] + "\n Nome : " + infoClient[1] + "\n Numero de telemovel : " + infoClient[2] + "\n Email : " + infoClient[3]);
+                    System.out.println(espacos + "\n ID : " + infoClient[0] + "\n Nome : " + infoClient[1] + "\n Numero de telemovel : " + infoClient[2] + "\n Email : " + infoClient[3]);
                     break;
-                case 5:
+                case 5: // Jogo mais caro
+                    String jogoMaiscaro = statisticsAllVendas()[2];
+                    System.out.println(espacos + "\nO jogo mais caro é " + jogoMaiscaro + "\n" + espacos + "$$$$$ Clientes que o compraram $$$$");
+                    String[][] matrizVendasJogoMaisCaro = searchForDataMatriz(matrizVendas, jogoMaiscaro, 4);           // procura na matriz das vendas quais as vendas que contem o jogo mais caro apenas
+                    for (int i = 0; i < matrizVendasJogoMaisCaro.length; i++) {
+                        String[] clienteVenda = searchForDataArray(matrizClientes, matrizVendasJogoMaisCaro[i][1], 0);
+                        System.out.println("\t" + "ID : " + clienteVenda[0] + "\t & Nome : " + clienteVenda[1]);
+                    }
                     break;
-                case 6:
+                case 6: // Clientes/Cliente mais gastador
+                    String[][] clientesMaisGastadores = listaClientesOrdemGasto();
+                    String[][] maisGastadores = searchForDataMatriz(clientesMaisGastadores, clientesMaisGastadores[0][1], 1);  // retorna o primeiro cliente que gastou mais e o resto que tenha gasto igual ao primeiro
+                    for (int i = 0; i < maisGastadores.length; i++) {
+                        String[][] vendasDoCliente = searchForDataMatriz(matrizVendas, maisGastadores[i][0], 1);
+                        String[] clienteInfo = searchForDataArray(matrizClientes, maisGastadores[i][0], 0);
+                        System.out.println("\n\t" + (i + 1) + ".Cliente *** Nome : " + clienteInfo[1] + "\t Contacto : " + clienteInfo[2] + "\t Email : " + clienteInfo[3]);
+                        System.out.println("\n\t ***** Jogos Compradados pelo " + clienteInfo[1] + " *****");
+                        String[] jogosComprados = columnDataWithoutRep(vendasDoCliente, 4);
+                        for (int k = 0; k < jogosComprados.length; k++){
+                            System.out.println("\t" + k+1 + ". " + jogosComprados[k]);
+                        }
+                    }
                     break;
                 case 7:
+                    System.out.println("\n\tA categoria mais lucrativa é : " + statisticsAllVendas()[4]);
+                    System.out.println("\tLucrou um total de " + statisticsAllVendas()[3] + "euros");
                     break;
                 case 8:
+                    System.out.println("\nIntroduza o nome do jogo que deseja pesquisar : ");
+                    String nomeJogo = input.next() + input.nextLine();
+                    if(searchIfExistsOnMatriz(matrizVendas, nomeJogo,4))
+                    {
+                        String[][] vendasDoJogo = searchForDataMatriz(matrizVendas, nomeJogo, 4);
+                        String[] clientesDoJogo = columnDataWithoutRep(vendasDoJogo, 1);
+                        System.out.println("\n\t ***** Clientes que compraram o jogo " + nomeJogo + " *****\n");
+                        for (int i = 0; i < clientesDoJogo.length; i++){
+                            String[] infoDoClient = searchForDataArray(matrizClientes, clientesDoJogo[i],0 );
+                            System.out.println("\t" + (i + 1) + " # Nome : "+infoDoClient[1] + "\t Email : " + infoDoClient[3] + "\t Contacto : " + infoDoClient[2]);
+                        }
+                    }
                     break;
                 case 9:
                     break;
@@ -169,9 +286,10 @@ public class main {
                 default:
                     System.out.println("Opção inválida!");
             }
-        }while (opcaoAdmin != 0);
+        } while (opcaoAdmin != 0);
     }
 
+    public static String espacos = "     ";
     public static String[][] matrizLogins;
     public static String[][] matrizVendas;
     public static String[][] matrizCategorias;
@@ -208,14 +326,14 @@ public class main {
                     System.out.println("\n!!!!!!!! Opção inválida !!!!!!!!");
                 }
             }
-            switch (opcaoAdminOuCliente){
-                case 1 :
+            switch (opcaoAdminOuCliente) {
+                case 1:
                     boolean loginValid = loginAdminValid(input);
                     while (!loginValid) {
                         System.out.println("Username/password inválido!");
                         loginValid = loginAdminValid(input);
                     }
-                    if (loginValid){
+                    if (loginValid) {
                         System.out.println("\n***** Bem vindo ******");
                         menuAdmin(input);
                     }
